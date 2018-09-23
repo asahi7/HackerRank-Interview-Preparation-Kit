d86e22dcd3480cebfad5c7df29be46f77a6a5a0e
Aibek Smagulov <smagulovaybek@gmail.com>
2018-09-23 21:44:19 +0900

    two-strings

---
+++ two-strings.java
@@ -0,0 +1,49 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < s1.length(); i++) {
+            set.add(s1.charAt(i));
+        }
+        for(int j = 0; j < s2.length(); j++) {
+            if(set.contains(s2.charAt(j))) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
