e85aa15796900d647a41b1d75ffd88c8eb8dc92d
Aibek Smagulov <smagulovaybek@gmail.com>
2018-09-26 00:48:46 +0900

    ctci-comparator-sorting

---
+++ ctci-comparator-sorting.java
@@ -0,0 +1,13 @@
+
+class Checker implements Comparator<Player> {
+  	// complete this method
+	public int compare(Player a, Player b) {
+    if(a.score < b.score) {
+      return 1;
+    } else if(a.score > b.score) {
+      return -1;
+    } else {
+      return a.name.compareTo(b.name) > 0 ? 1 : -1;
+    }
+  }
+}
