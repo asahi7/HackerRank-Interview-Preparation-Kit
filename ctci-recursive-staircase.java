5922aa385e263c0d137095cc714ceead96469d90
Aibek Smagulov <smagulovaybek@gmail.com>
2018-09-29 22:27:31 +0900

    ctci-recursive-staircase

---
+++ ctci-recursive-staircase.java
@@ -0,0 +1,45 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int stepPerms(int n) {
+      int[] memo = new int[50];
+      memo[0] = 1;
+      for(int i = 0; i < n; i++) {
+        memo[i + 1] += memo[i];
+        memo[i + 2] += memo[i];
+        memo[i + 3] += memo[i];
+      }
+      return memo[n];
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int s = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int sItr = 0; sItr < s; sItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int res = stepPerms(n);
+
+            bufferedWriter.write(String.valueOf(res));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
